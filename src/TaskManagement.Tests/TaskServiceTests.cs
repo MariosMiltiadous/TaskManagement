81fd3988d94b3fb134183253bd91bd5414ab7aef
Marios <46002841+MariosMiltiadous@users.noreply.github.com>
2025-02-13 22:58:46 +0200

    Initial test commit

---
+++ src/TaskManagement.Tests/TaskServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagement.Api.Data;
+using TaskManagement.Api.Services;
+
+namespace TaskManagement.Tests;
+
+public class TaskServiceTests
+{
+    private readonly TaskService _service;
+    private readonly ApplicationDbContext _context;
+
+    public TaskServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                      .UseInMemoryDatabase(databaseName: "TestDB").Options;
+        _context = new ApplicationDbContext(options);
+        _service = new TaskService(_context);
+    }
+
+    [Fact]
+    public async Task GetTasksAsync_ShouldReturnEmptyList_WhenNoTasksExist()
+    {
+        var result = await _service.GetTasksAsync();
+        Assert.Empty(result);
+    }
+}
